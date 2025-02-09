c657781cbaaad19c2e921b7fb924857729841323
Anup Shakya <dev2@krizmatic.com>
2025-02-09 15:11:31 +0545

    Test Email Command Added in Code

---
+++ app/Console/Commands/TestMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\AcledErrorNotification;
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class TestMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This is a Command to test mail.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try{
+            Mail::to('anupshk7@gmail.com')->send(new AcledErrorNotification(
+                'Test Mail',
+                'For Testing Purpose',
+                'onLY FOR TEsting'
+            ));
+            Log::channel('acled_data_log')->info('Test Email sent successfully!!!');
+        }catch(Exception $e){
+            Log::channel('acled_data_log')->error('Failed to Send Test Email: '.$e->getMessage());
+        }
+    }
+}
